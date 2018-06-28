22b4e96c86b39b0761757e7dd66a0451a101c314
Edwin <edwinrmondragon@gmail.com>
2018-06-28 00:20:54 -0500

    funcionalidad de la app
    se agrega arreglos asi como el doble enlace de datos entre otras directivas utilizadas

--- crud/src/app/app.component.ts
+++ crud/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+import { Empleados } from './models/empleados'; //importo la clase
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  empleadosArray:Empleados[] = [ //arreglo de empleados
+{id: 1, nombre:"Edwin", ciudad:"Queretaro"},
+{id: 2, nombre:"Raul", ciudad:"Queretaro"},
+{id: 3, nombre:"Cesar", ciudad:"Queretaro"},
+  ];
+
+selectedEmpleado: Empleados = new Empleados();
+
+Editar(empleado:Empleados){
+  this.selectedEmpleado=empleado; // sera igual al empleado seleccionado
+}
+
+agregareditar(){
+if(this.selectedEmpleado.id === 0){
+  this.selectedEmpleado.id = this.empleadosArray.length + 1 ;
+  this.empleadosArray.push(this.selectedEmpleado);
+}
+  this.selectedEmpleado = new Empleados();
+}
+
+eliminar(){
+  if(confirm('Eliminar empleado?')){
+    this.empleadosArray = this.empleadosArray.filter(x => x != this.selectedEmpleado);//filtrar empleado 
+    this.selectedEmpleado = new Empleados();
+  }
+ 
+}
 }
